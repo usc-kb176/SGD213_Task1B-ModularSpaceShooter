2b5bd38832b1f21e77a64174edb823888908603d
usc-kb176 <k_b176@student.usc.edu.au>
2024-03-27 00:44:27 +1000

    Renamed BulletMoveForward to BulletMovement

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private float acceleration = 50f;
+    private float initialVelocity = 5f;
+    private Vector2 movementDirection = Vector2.up;
+
+    public MovementScript movement;
+
+    private void Start()
+    {
+        movement = GetComponent<MovementScript>();
+        movement.RigidbodySetup(initialVelocity, movementDirection);
+    }
+
+    private void Update()
+    {
+        movement.MoveObject(acceleration, movementDirection);
+    }
+
+}
