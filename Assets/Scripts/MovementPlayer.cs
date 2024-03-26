3dfc0769a21757263c861aa28e393a54d402c732
usc-kb176 <k_b176@student.usc.edu.au>
2024-03-27 02:00:43 +1000

    Added movement via MovementScript

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -3,32 +3,20 @@
 public class MovementPlayer : MonoBehaviour
 {
     // SerializeField exposes this value to the Editor, but not to other Scripts!
-    // It is "pseudo public"
-    // HorizontalPlayerAcceleration indicates how fast we accelerate Horizontally
     [SerializeField]
     private float acceleration = 5000f;
+    private Vector2 movementDirection = Vector2.right;
 
-    private Rigidbody2D rb;
+    private MovementScript movement;
 
-    // Use this for initialization
     void Start()
     {
-        // Get OurRigidbodyComponent once at the start of the game and store a reference to it
-        // This means that we don't need to call GetComponent more than once! This makes our game faster. (GetComponent is SLOW)
-        rb = GetComponent<Rigidbody2D>();
+        movement = GetComponent<MovementScript>();
+        movement.RigidbodySetup();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-
-        if (horizontalInput != 0.0f)
-        {
-            Vector2 ForceToAdd = Vector2.right * horizontalInput * acceleration * Time.deltaTime;
-            rb.AddForce(ForceToAdd);
-            //print(HorizontalInput);
-        }
+        movement.MovePlayer(acceleration, movementDirection);
     }
-
 }
