cc854fe85c7fbe6610b9dbdcc50684501158f6a0
usc-kb176 <k_b176@student.usc.edu.au>
2024-03-27 00:58:44 +1000

    Renamed EnemyMoveForward.cs to EnemyMovement.cs

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private float acceleration = 75f;
+    private float initialVelocity = 2f;
+    private Vector2 movementDirection = Vector2.down;
+
+    public MovementScript movement;
+
+    private void Start()
+    {
+        movement = GetComponent<MovementScript>();
+        movement.RigidbodySetup(initialVelocity, movementDirection);
+    }
+
+    private void Update()
+    {
+        movement.MoveObject(acceleration, movementDirection);
+    }
+}
