2fd37b6bbb5e342e5418adfe0911cf4c60f385ca
usc-kb176 <k_b176@student.usc.edu.au>
2024-03-30 23:16:48 +1000

    Created a modular script out of ShootingScript.cs

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -12,15 +12,21 @@ public class ShootingScript : MonoBehaviour
     private float lastFiredTime = 0f;
     private float bulletOffset = 2f;
 
-    void Start()
+    /// <summary>
+    /// SetupBulletOffset() - Setup the offset of the bullet so it doesn't spawn
+    /// ontop of the spaceship
+    /// </summary>
+    public void SetupBulletOffset()
     {
         // Do some math to perfectly spawn bullets in front of us
         bulletOffset = GetComponent<Renderer>().bounds.size.y / 2 // Half of our size
             + bullet.GetComponent<Renderer>().bounds.size.y / 2; // Plus half of the bullet size
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// ShootBullet() - Fire a bullet when the user presses the fire button
+    /// </summary>
+    public void ShootBullet()
     {
         if (Input.GetButton("Fire1"))
         {
@@ -30,11 +36,11 @@ public class ShootingScript : MonoBehaviour
             if (currentTime - lastFiredTime > fireDelay)
             {
                 Vector2 spawnPosition = new Vector2(transform.position.x, transform.position.y + bulletOffset);
-
                 Instantiate(bullet, spawnPosition, transform.rotation);
-
                 lastFiredTime = currentTime;
             }
         }
     }
+
+
 }
